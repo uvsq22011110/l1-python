8cec0b6762bba1e561ecbc29785d2b837af59336
uvsq22011110 <uvsq22011110@users.noreply.github.com>
2020-11-26 19:36:13 +0100

    TD4

--- exercises/TD04_listes/syracuse.py
+++ exercises/TD04_listes/syracuse.py
@@ -1,1 +1,11 @@
-#TD4
+def syracuse(n):
+    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
+    while n != 0:
+        if n % 2 == 1:
+            n = n * 3 + 1
+        else:
+            n = n / 2
+    return n
+
+
+print(syracuse(3))
